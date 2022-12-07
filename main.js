417dd5e40f6936445a1a96d66ea90d6719d8551f
Ruslan <ruslanmehdiyev922@gmail.com>
2022-12-07 17:52:16 +0400

    finished

---
+++ main.js
@@ -0,0 +1,14 @@
+const BASE_URL = "https://northwind.vercel.app/api/supppliers";
+
+async function retryFetch(url, n) {
+  try {
+    return await axios.get(url);
+  } catch (err) {
+    if (n === 1) {
+      throw err;
+    }
+    return await retryFetch(url, n - 1);
+  }
+}
+
+console.log(retryFetch(BASE_URL, 6));
